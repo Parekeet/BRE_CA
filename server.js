648b7c22ee5fd8513031e1c540f03b91619a2805
Patcharin Sakulkoo <psakulkoo@gmail.com>
2016-10-24 07:16:02 -0700

    adding transparent CSS background and working nodemailer

--- server.js
+++ server.js
@@ -57,46 +57,4 @@ var allowCrossDomain = function(req, res, next) {
 
 app.use(allowCrossDomain);
 
-///////////////////////////////////////////////////////////
-//                     R O U T E S                       //
-///////////////////////////////////////////////////////////
-// router.route('/properties')
-
-  //POST
-  // .post(function(req, res) {
-  //   var property  = new Property(); //Creates new instance of property model
-  //   property.name = req.body.name;  //Sets the property name
-  //   //save the property and checks for errors
-  //   property.save(function(err) {
-  //     if (err)
-  //       res.send(err);
-  //     res.json('Property has been created!');
-  //   });
-  // })
-
-  //GET all properties
-  // .get(function(req, res) {
-  //   Property.find(function(err, properties) {
-  //     if(err)
-  //       res.send(err);
-  //     res.json(properties);
-  //   });
-  // });
-
-// router.route('/properties/:property_id')
-
-  // GET BY ID
-  // .get(function(req, res) {
-  //   MONGO USES KEY:VALUE PAIRING
-  //   WE ARE USING {} BECAUSE IT IS AN OBJECT
-  //   WE USE BODY WHEN WE SEND INFO (POST)
-  //   WE USE PARAMS WHEN WE ARE GRABBING SOMETHING FROM THE URL
-  //   Property.findById({_id: req.params.property_id}, function(err, property){
-  //     if (err)
-  //       res.send(err);
-  //     res.json(property);
-  //   });
-  // });
-
-///////////////////////////////////////////////////////////
 
